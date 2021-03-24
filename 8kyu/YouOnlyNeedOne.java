fc4133084df28e00e6c2b12c62ad7c63fa47aa6d
Thays <thayscasado@hotmail.com>
2021-03-24 00:44:29 -0700

    8 kyu

---
+++ 8kyu/YouOnlyNeedOne.java
@@ -0,0 +1,16 @@
+public class YouOnlyNeedOne {
+
+  public static boolean check(Object[] a, Object x) {
+      // Your code here
+  boolean flag = false;
+      for(int i = 0; i < a.length; i++) {
+        if(a[i].equals(x)) {
+          flag = true;
+          break;
+        } else { 
+          flag = false;
+        }
+      }	      
+      return flag;
+    }
+}
