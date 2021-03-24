7f9e583e6555dface695416bd6105febf971ee34
Thays <thayscasado@hotmail.com>
2021-03-24 09:30:06 -0700

    you only need one

--- kyu8/YouOnlyNeedOne.java
+++ kyu8/YouOnlyNeedOne.java
@@ -1,6 +1,12 @@
 package kyu8;
 public class YouOnlyNeedOne {
 
+  public static void main(String[] args) {
+    System.out.println(check(new Object[] {80, 117, 115, 104, 45, 85, 112, 115}, 45));
+  }
+
+ 
+
   public static boolean check(Object[] a, Object x) {
       // Your code here
   boolean flag = false;
@@ -15,3 +21,10 @@ public class YouOnlyNeedOne {
       return flag;
     }
 }
+
+/**
+ * 
+ *  public static boolean check(Object[] a, Object x) {
+    return java.util.Arrays.asList(a).contains(x);
+}
+ */
